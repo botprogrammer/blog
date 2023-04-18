04167c1bacd883dd8330a0b19fffdbaef6853cd7
botprogrammer <p03goswami@gmail.com>
2023-04-18 08:23:11 +0530

    initialpropsreplacementcommit

--- pages/home.js
+++ pages/home.js
@@ -1,7 +1,6 @@
 import { NextSeo } from "next-seo";
 import Layout from "@components/layout";
 import Container from "@components/container";
-import { useRouter } from "next/router";
 import PostList from "@components/postlist";
 
 import { useQuery, gql } from "@apollo/client";
@@ -42,8 +41,8 @@ export default function Home({ url }) {
       <NextSeo
         title="Pranav Goswami Blogs"
         description="Welcome to Pranav Goswami Blogs, where I love to share my passion for all things web development, including React JS, Next JS, Gatsby, Javascript, UI/UX, and more. This is the perfect place to find expert insights, helpful tips, and engaging tutorials on the latest trends and technologies in the field. Whether you're just starting out or looking to take your skills to the next level, I've got you covered. So come on in, have a look around, and join me in this journey of exploring the exciting world of web development!"
-        image=""
-        url=""
+        image="/img/og/home_og.png"
+        url={url}
         canonical={url}
       />
       {data && (
--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,24 @@ export default function Index({ fullUrl }) {
   return <Home url={fullUrl} />;
 }
 
-Index.getInitialProps = async ({ req }) => {
-  const protocol = req.headers["x-forwarded-proto"] || "http";
-  const host = req.headers["host"];
-  const fullUrl = `${protocol}://${host}${req.url}`;
+// Index.getInitialProps = async context => {
+//   console.log({ context });
+
+//   // const protocol = req.headers["x-forwarded-proto"] || "http";
+//   // const host = req.headers["host"];
+//   // const fullUrl = `${protocol}://${host}${req.url}`;
+
+//   return {
+//     // fullUrl
+//   };
+// };
+
+export const getServerSideProps = async context => {
+  console.log({ context });
 
   return {
-    fullUrl
+    props: {
+      data: null
+    }
   };
 };
