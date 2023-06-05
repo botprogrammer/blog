8d504128b9daa8ea79a0c18bb55a07ac0df6a874
botprogrammer <p03goswami@gmail.com>
2023-06-05 08:31:20 +0530

    Added google verification

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import Home from "./home";
+import Head from "next/head";
 
 export default function Index() {
+  <Head>
+    <meta
+      name="google-site-verification"
+      content="Wj6qD4-SzJNYZdZwRqOhdIaWwmc9lVyHFogxKMzIL9E"
+    />
+  </Head>;
   return <Home />;
 }
