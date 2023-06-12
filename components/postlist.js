276c13656fdda4444cd575673bd2631f6fea75c7
botprogrammer <p03goswami@gmail.com>
2023-06-12 22:15:01 +0530

    updated link tags for better SEO

--- components/postlist.js
+++ components/postlist.js
@@ -43,44 +43,49 @@ export default function PostList({ post, aspect }) {
   return (
     <>
       <div className="cursor-pointer group">
-        <div
-          className={cx(
-            "relative overflow-hidden transition-all bg-gray-100 rounded-md dark:bg-gray-800 hover:scale-105",
-            aspect === "landscape" ? "aspect-video" : "aspect-square"
-          )}>
-          <Link href={`/post/${data?.postSlug}`}>
-            {data ? (
-              <Image
-                src={data.postImage}
-                blurDataURL={data.postImage}
-                placeholder="blur"
-                sizes="80vw"
-                layout="fill"
-                objectFit="cover"
-                className="transition-all"
-                alt="Image"
-              />
-            ) : (
-              <span className="absolute w-16 h-16 text-gray-200 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-                <PhotographIcon />
-              </span>
-            )}
-          </Link>
-        </div>
+        <Link href={`/post/${data?.postSlug}`}>
+          <a>
+            <div
+              className={cx(
+                "relative overflow-hidden transition-all bg-gray-100 rounded-md dark:bg-gray-800 hover:scale-105",
+                aspect === "landscape"
+                  ? "aspect-video"
+                  : "aspect-square"
+              )}>
+              {data ? (
+                <Image
+                  src={data.postImage}
+                  blurDataURL={data.postImage}
+                  placeholder="blur"
+                  sizes="80vw"
+                  layout="fill"
+                  objectFit="cover"
+                  className="transition-all"
+                  alt="Image"
+                />
+              ) : (
+                <span className="absolute w-16 h-16 text-gray-200 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                  <PhotographIcon />
+                </span>
+              )}
+            </div>
+          </a>
+        </Link>
 
         <CategoryLabel categories={post.tags} />
+
         <h2 className="mt-2 text-lg font-semibold tracking-normal text-brand-primary dark:text-white">
-          <Link href={`/post/${data?.postSlug}`}>
-            <div>
-              {data?.postTitle}{" "}
-              <span className="text-xs text-gray-300 dark:text-gray-600 mx-2">
-                &bull;
-              </span>
-              <span className="text-sm font-light text-gray-400">
-                {data?.postReadingTime} min read
-              </span>
-            </div>
-          </Link>
+          <div>
+            <Link href={`/post/${data?.postSlug}`}>
+              <a>{data?.postTitle}</a>
+            </Link>
+            <span className="text-xs text-gray-300 dark:text-gray-600 mx-2">
+              &bull;
+            </span>
+            <span className="text-sm font-light text-gray-400">
+              {data?.postReadingTime} min read
+            </span>
+          </div>
         </h2>
 
         <div className="hidden">
